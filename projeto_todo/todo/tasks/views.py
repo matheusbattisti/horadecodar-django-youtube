1eeefe64b3507992a473ce09faade7021e2c0dca
Matheus Battisti <matheusbudkewicz@gmail.com>
2019-02-11 22:17:44 -0200

    finalizando aula 3

--- projeto_todo/todo/tasks/views.py
+++ projeto_todo/todo/tasks/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def listaDeTarefas(request):
-    return HttpResponse('Lista de tarefas')
+def helloWorld(request):
+    return HttpResponse('Hello World!')
