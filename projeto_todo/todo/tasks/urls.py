5df7d97efc016ccee3ea6a568f7cb0549703b4d2
Matheus Battisti <matheusbudkewicz@gmail.com>
2019-02-11 22:34:50 -0200

    views, templates e URLs

--- projeto_todo/todo/tasks/urls.py
+++ projeto_todo/todo/tasks/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.helloWorld),
+    path('helloworld/', views.helloWorld),
+    path('', views.taskList, name='task-list'),
+    path('yourname/<str:name>', views.yourName, name='your-name'),
 ]
--- projeto_todo/todo/tasks/views.py
+++ projeto_todo/todo/tasks/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def helloWorld(request):
     return HttpResponse('Hello World!')
+
+def taskList(request):
+    return render(request, 'tasks/list.html')
+
+def yourName(request, name):
+    return render(request, 'tasks/yourname.html', {'name':name})
