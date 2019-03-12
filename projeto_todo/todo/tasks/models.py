92a2a03fb04402da7a38eb1657d6e9afb26297f8
Matheus Battisti <matheusbudkewicz@gmail.com>
2019-03-11 21:56:08 -0300

    finalizando models

--- projeto_todo/todo/tasks/models.py
+++ projeto_todo/todo/tasks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+
+    STATUS = (
+        (1, 'Doing'),
+        (2, 'Done'),
+    )
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    done = models.CharField(
+        max_length=1,
+        choices=STATUS,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
